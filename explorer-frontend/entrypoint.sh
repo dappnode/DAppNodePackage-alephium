d571d7394856bf3295668f5b9e65c0f42f7ba011
mgarciate <mgarciate@gmail.com>
2022-10-05 12:37:24 +0200

    Add entrypoint to explorer services

--- explorer-frontend/entrypoint.sh
+++ explorer-frontend/entrypoint.sh
@@ -8,4 +8,4 @@ else
     exit 1
 fi
 
-exec /usr/lib/postgresql/14/bin/postgres -D /var/lib/postgresql/14/main -c config_file=/etc/postgresql/14/main/postgresql.conf
+exec node /usr/local/bin/serve -s build -p 3000
