c64edcde27be35d29a47bf5a638b3273d7283597
mgarciate <mgarciate@gmail.com>
2022-11-15 17:11:47 +0100

    Fix #40 Make starting from snapshot optional, disabled by default

--- alephium/entrypoint.sh
+++ alephium/entrypoint.sh
@@ -5,17 +5,12 @@ ALEPHIUM_NETWORK=${ALEPHIUM_NETWORK:-mainnet}
 
 # snapshot-loader.sh originally available at from https://github.com/touilleio/alephium-standalone
 # Call snapshot-loader.sh and ensure it completed successfully, stopping the execution otherwise.
-if ! ./snapshot-loader.sh
-then
-  echo "Loading the snapshot failed. See logs above for more details, apply recommended actions and retry"
-  exit 1
-fi
-
-# Copy default user.conf if it does not exists already
-if [ ! -f "$ALEPHIUM_HOME/user.conf" ]
-then
-    echo "Copying standalone user.conf file"
-    cp "/user-$ALEPHIUM_NETWORK.conf" "$ALEPHIUM_HOME/user.conf"
+if [ $INITIALIZING_FROM_SNAPSHOT = "enabled" ]; then
+    if ! ./snapshot-loader.sh
+    then
+        echo "Loading the snapshot failed. See logs above for more details, apply recommended actions and retry"
+        exit 1
+    fi
 fi
 
 echo "Starting java -jar /alephium.jar ${JAVA_NET_OPTS} ${JAVA_MEM_OPTS} ${JAVA_GC_OPTS} ${JAVA_EXTRA_OPTS} ${EXTRA_OPTS} $@"
